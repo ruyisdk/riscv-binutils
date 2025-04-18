1a4ae426493046b4a9e64db08c8e5f26eb989c9c
Dongyan Chen <chendongyan@isrc.iscas.ac.cn>
2025-04-18 16:48:57 +0800

    RISC-V: Add support for zama16b extension.
    This implements the zama16b extension, version 1.0[1].
    
    [1]https://github.com/riscv/riscv-profiles/blob/main/src/rva23-profile.adoc
    
    bfd/ChangeLog:
    
    	* elfxx-riscv.c: Handle zama16b.
    
    gas/ChangeLog:
    
    	* NEWS: Updated.
    	* testsuite/gas/riscv/march-help.l: Updated.

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -1486,6 +1486,7 @@ static const struct riscv_supported_ext riscv_supported_std_z_ext[] =
   {"zabha",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"zacas",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"zalasr",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
+  {"zama16b",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"zalrsc",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"zawrs",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"zfbfmin",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
